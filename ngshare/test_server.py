4039fec7352e7cd285c52deff8f973ce4dd8fed7
Eric Li <ercli@ucdavis.edu>
2020-03-02 23:47:23 -0800

    ngshare: initialize database API

--- ngshare/test_server.py
+++ ngshare/test_server.py
@@ -8,7 +8,7 @@ import os
 import argparse
 from urllib.parse import urlparse
 
-import base64, binascii
+import base64, binascii, datetime
 
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
@@ -211,6 +211,47 @@ class Favicon(MyRequestHandler):
         file_name = os.path.join(pwd, 'favicon.ico')
         self.write(open(file_name, 'rb').read())
 
+class InitDatabase(MyRequestHandler):
+    @authenticated
+    def get(self):
+        # Dangerous: do not use in production
+        db = self.db
+        db.query(User).delete()
+        db.query(Course).delete()
+        db.query(Assignment).delete()
+        db.query(Submission).delete()
+        db.query(File).delete()
+        uk = User('Kevin')
+        ua = User('Abigail')
+        ul = User('Lawrence')
+        ue = User('Eric')
+        course1 = Course('course1', uk)
+        course2 = Course('course2', ua)
+        db.add(course1)
+        db.add(course2)
+        course1.students.append(ul)
+        course2.students.append(ue)
+        aa = Assignment('assignment2a', course2)
+        ab = Assignment('assignment2b', course2)
+        ac = Assignment('challenge', course1)
+        db.add(aa)
+        db.add(ab)
+        db.add(ac)
+        s1 = Submission(ul, ac)
+        s2 = Submission(ul, ac)
+        s1.timestamp = datetime.datetime(2020, 1, 1, 0, 0, 0, 0)
+        s1.random = '12345678-90ab-cdef-0123-456789abcdef'
+        db.add(s1)
+        db.add(s2)
+        aa.files.append(File('file0', b'00000'))
+        ab.files.append(File('file1', b'11111'))
+        ac.files.append(File('file2', b'22222'))
+        s1.files.append(File('file3', b'33333'))
+        s2.files.append(File('file4', b'44444'))
+        s1.feedbacks.append(File('file5', b'55555'))
+        db.commit()
+        self.json_success('done')
+
 class ListCourses(MyRequestHandler):
     '/api/courses'
     @authenticated
@@ -298,6 +339,7 @@ def main():
             (prefix + 'course/([^/]+)', AddCourse),
             (prefix + 'assignments/([^/]+)', ListAssignments),
             (prefix + 'assignment/([^/]+)/([^/]+)', DownloadReleaseAssignment),
+            (prefix + 'initialize-Data6ase', InitDatabase),
             (r'.*', Test404Handler),
         ],
         autoreload=True
