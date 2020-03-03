a662944a40d9d3be6039cbd07b96e2d53bdfa8f9
Eric Li <ercli@ucdavis.edu>
2020-03-02 22:37:00 -0800

    Port AddCourse with some helper functions

--- ngshare/test_server.py
+++ ngshare/test_server.py
@@ -26,37 +26,91 @@ Session = sessionmaker(bind=engine)
 # pylint: disable=invalid-name
 # pylint: disable=abstract-method
 
-class MyRequestHandler(RequestHandler):
-    '''
-        Custom request handler for ngshare
-    '''
+class MyHelpers:
+    'Helper functions for database accesses'
+    def strftime(self, dt):
+        return dt.strftime('%Y-%m-%d %H:%M:%S.%f %Z')
+
+    def strptime(self, string):
+        0/0
+
+    def path_check(self, pathname):
+        0/0
+
+    def find_course(db, course_id) :
+        'Return a Course object from id, or raise error'
+        course = db.query(Course).filter(Course.id == course_id).one_or_none()
+        if course is None :
+            self.json_error('Course not found')
+        return course
+
+    def check_course_student(db, course, user) :
+        'Assert user is a student in the course'
+        self.json_error('Permission denied 0/0')
+        0/0
+        if not is_course_student(db, course, user) :
+            self.json_error('Permission denied (not course student)')
+            0/0
+
+    def check_course_instructor(db, course, user) :
+        'Assert user is an instructor in the course'
+        if not is_course_instructor(db, course, user) :
+            self.json_error('Permission denied (not course instructor)')
+            0/0
+
+    def check_course_related(db, course, user) :
+        'Assert user is a student or an instructor in the course'
+        if not is_course_instructor(db, course, user) and \
+            not is_course_student(db, course, user) :
+            self.json_error('Permission denied (not related to course)')
+            0/0
+
+class MyRequestHandler(HubAuthenticated, RequestHandler, MyHelpers):
+    'Custom request handler for ngshare'
     def json_success(self, msg=None, **kwargs):
-        '''
-            Return success as a JSON object
-        '''
+        'Return success as a JSON object'
         assert 'success' not in kwargs and 'message' not in kwargs
         resp = {'success': True, **kwargs}
         if msg is not None:
             resp['message'] = msg
         self.finish(json.dumps(resp))
+
+    def json_error(self, msg, **kwargs) :
+        'Return error as a JSON object'
+        assert 'success' not in kwargs and 'message' not in kwargs
+        self.finish(json.dumps({'success': False, 'message': msg, **kwargs}))
+
     def prepare(self):
         'Provide a db object'
         self.db = Session()
+        self.user = User.from_jupyterhub_user(self.get_current_user(), self.db)
+
     def on_finish(self):
         self.db.close()
 
-class ListCourses(HubAuthenticated, MyRequestHandler):
+class ListCourses(MyRequestHandler):
     '/api/courses'
     @authenticated
     def get(self):
         'List all available courses the user is taking or teaching (anyone)'
-        user = User.from_jupyterhub_user(self.get_current_user(), self.db)
         courses = set()
-        for i in user.teaching:
+        for i in self.user.teaching:
             courses.add(i.id)
-        for i in user.taking:
+        for i in self.user.taking:
             courses.add(i.id)
-        return self.json_success(courses=sorted(courses))
+        self.json_success(courses=sorted(courses))
+
+class AddCourse(MyRequestHandler):
+    '/api/course/<course_id>'
+    @authenticated
+    def post(self, course_id):
+        'Add a course (anyone)'
+        if self.db.query(Course).filter(Course.id == course_id).one_or_none() :
+            self.json_error('Course already exists')
+        course = Course(course_id, self.user)
+        self.db.add(course)
+        self.db.commit()
+        self.json_success()
 
 class TestCreateCourseHandler(HubAuthenticated, RequestHandler):
     @authenticated
@@ -105,6 +159,8 @@ def main():
              TestCreateCourseHandler),
             (os.environ['JUPYTERHUB_SERVICE_PREFIX'] + 'courses',
              ListCourses),
+            (os.environ['JUPYTERHUB_SERVICE_PREFIX'] + 'course/([^/]+)',
+             AddCourse),
             (r'.*', Test404Handler),
         ],
         autoreload=True
