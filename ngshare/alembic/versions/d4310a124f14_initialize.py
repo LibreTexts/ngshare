eb4faa73268f5dff4d280f5977dce3ec215bc8df
Eric Li <ercli@ucdavis.edu>
2020-04-19 23:35:17 -0700

    Initialize alembic

---
+++ ngshare/alembic/versions/d4310a124f14_initialize.py
@@ -0,0 +1,24 @@
+"""Initialize
+
+Revision ID: d4310a124f14
+Revises: 
+Create Date: 2020-04-19 23:27:47.968611
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd4310a124f14'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
