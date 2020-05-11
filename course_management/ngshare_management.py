581bdf5862d22598a5f88e841e25611468ac23b2
aalmanza1998 <aalmanza@ucdavis.edu>
2020-05-11 11:35:28 -0700

    some style changes

--- course_management/ngshare_management.py
+++ course_management/ngshare_management.py
@@ -14,12 +14,15 @@ def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
 def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
 
+
 class User:
+
     def __init__(self, id, first_name, last_name, email):
         self.id = id
         self.first_name = '' if first_name is None else first_name
         self.last_name = '' if last_name is None else last_name
         self.email = '' if email is None else email
+
 
 def print_usage():
     prYellow('-----------------------------------------------------USAGE EXAMPLES-----------------------------------------------------')
@@ -28,32 +31,34 @@ def print_usage():
     print('     python3 ngshare_management.py create_course -c math101')
     prCyan('Add one student to a course:')
     print('     python3 ngshare_management.py add_student --course_id=math101  --student_id=12345 --first_name=jane --last_name=doe --email=jdoe@mail.com')
-    print('     python3 ngshare_management.py add_student -c math101 -s 12345 -f jane -l doe -e jdoe@mail.com') 
+    print('     python3 ngshare_management.py add_student -c math101 -s 12345 -f jane -l doe -e jdoe@mail.com')
     prRed('     first name, last name, and email are optional parameters')
     prCyan('Add multiple students to a course:')
     print('     python3 ngshare_management.py add_students --course_id=math101 --students_csv=math101Students.csv')
     print('     python3 ngshare_management.py add_students -c math101 --students_csv=math101Students.csv')
     prRed('     the csv file must have the following columns: \'student_id\', \'first_name\', \'last_name\', \'email\'')
     prCyan('Remove student from a course:')
-    print('     python3 ngshare_management.py remove_student --course_id=math101  --student_id=12345')   
+    print('     python3 ngshare_management.py remove_student --course_id=math101  --student_id=12345')
     print('     python3 ngshare_management.py remove_student -c math101  -s 12345')
     prCyan('Add instructor to a course:')
     print('     python3 ngshare_management.py add_instructor --course_id=math101  --instructor_id=12345 --first_name=jane --last_name=doe --email=jdoe@mail.com')
-    print('     python3 ngshare_management.py add_instructor -c math101 -i 12345 -f jane -l doe -e jdoe@mail.com') 
+    print('     python3 ngshare_management.py add_instructor -c math101 -i 12345 -f jane -l doe -e jdoe@mail.com')
     prRed('     first name, last name, and email are optional parameters')
     prCyan('Remove instructor from a course:')
-    print('     python3 ngshare_management.py remove_instructor --course_id=math101  --instructor_id=12345')   
+    print('     python3 ngshare_management.py remove_instructor --course_id=math101  --instructor_id=12345')
     print('     python3 ngshare_management.py remove_instructor -c math101  -i 12345\n')
     prGreen('You can add the --jhub flag at the end of any command to execute the same action in JupyterHub')
     prGreen('For example running \'python3 ngshare_management.py create_course --course_id=math101 --jhub\' creates the course in JupyterHub and ngshare')
     prYellow('------------------------------------------------------------------------------------------------------------------------')
     sys.exit(2)
 
+
 def get_username():
     if 'JUPYTERHUB_USER' in os.environ:
-        return os.environ['JUPYTERHUB_USER']  
+        return os.environ['JUPYTERHUB_USER']
     else:
         return os.environ['USER']
+
 
 def ngshare_url():
     if 'PROXY_PUBLIC_SERVICE_HOST' in os.environ:
@@ -62,16 +67,19 @@ def ngshare_url():
         # replace this with correct URL if you are not using a kubernetes set up
         return 'http://127.0.0.1:12121/api'
 
+
 def get_header():
     if 'JUPYTERHUB_API_TOKEN' in os.environ:
         return {'Authorization': 'token ' + os.environ['JUPYTERHUB_API_TOKEN']}
     else:
         return None
 
+
 def chown_to_user(path):
     uid = pwd.getpwnam(get_username()).pw_uid
     gid = grp.getgrnam(get_username()).gr_gid
     os.chown(path, uid, gid)
+
 
 # https://stackoverflow.com/questions/1770209/run-child-processes-as-different-user-from-a-long-running-python-process/6037494#6037494
 def demote(user_uid, user_gid):
@@ -80,23 +88,25 @@ def demote(user_uid, user_gid):
         os.setuid(user_uid)
     return result
 
+
 # https://stackoverflow.com/questions/1770209/run-child-processes-as-different-user-from-a-long-running-python-process/6037494#6037494
 def run_as_user(user_name, cwd, *args):
     pw_record = pwd.getpwnam(user_name)
-    user_name      = pw_record.pw_name
-    user_home_dir  = pw_record.pw_dir
-    user_uid       = pw_record.pw_uid
-    user_gid       = pw_record.pw_gid
+    user_name = pw_record.pw_name
+    user_home_dir = pw_record.pw_dir
+    user_uid = pw_record.pw_uid
+    user_gid = pw_record.pw_gid
     cwd = user_home_dir if cwd == '~' else cwd
     env = os.environ.copy()
-    env[ 'HOME'     ] = user_home_dir
-    env[ 'LOGNAME'  ] = user_name
-    env[ 'PWD'      ] = cwd
-    env[ 'USER'     ] = user_name
+    env['HOME'] = user_home_dir
+    env['LOGNAME'] = user_name
+    env['PWD'] = cwd
+    env['USER'] = user_name
     process = subprocess.Popen(
         args, preexec_fn=demote(user_uid, user_gid), cwd=cwd, env=env
     )
     return process.wait()
+
 
 def check_status_code(response):
     if response.status_code != requests.codes.ok:
@@ -109,8 +119,9 @@ def check_message(response):
     if not response['success']:
         prRed(response['message'])
         return None
-    
+
     return response
+
 
 def post(url, data):
     header = get_header()
@@ -125,7 +136,8 @@ def post(url, data):
         check_status_code(response)
         return None
 
-    return check_message(response)  
+    return check_message(response)
+
 
 def delete(url, data):
     header = get_header()
@@ -136,8 +148,9 @@ def delete(url, data):
     except Exception:
         check_status_code(response)
         return None
-    
+
     return check_message(response)
+
 
 def create_course(course_id, jhub):
     prGreen('Creating ngshare course {}'.format(course_id))
@@ -155,11 +168,12 @@ def create_course(course_id, jhub):
     if jhub:
         create_jh_course(course_id)
 
+
 def create_jh_course(course_id):
     prGreen('Creating JupyterHub course {}'.format(course_id))
 
     # create course root directory
-    course_root_dir =  '/home/{}/{}'.format(get_username(),course_id)
+    course_root_dir =  '/home/{}/{}'.format(get_username(), course_id)
     if not os.path.exists(course_root_dir):
         os.mkdir(course_root_dir)
     else:
@@ -189,7 +203,7 @@ def create_jh_course(course_id):
     prGreen('Successfully created JupyterHub course {}.'.format(course_id))
 
 
-def add_student(course_id, student:User, jhub):
+def add_student(course_id, student: User, jhub):
     # add student to ngshare
     url = '{}/student/{}/{}'.format(ngshare_url(), course_id, student.id)
     data = {'user': get_username(), 'first_name': student.first_name, 'last_name': student.last_name, 'email': student.email}
@@ -205,16 +219,19 @@ def add_student(course_id, student:User, jhub):
     if jhub:
         add_jh_student(course_id, student)
 
-def add_jh_student(course_id, student:User):
-    # add student to nbgrader database
-    ret = run_as_user(get_username(), os.getcwd(), 'nbgrader', 'db', 'student', 'add', '--first-name', student.first_name, '--last-name',student.last_name, '--email', student.email, student.id)
 
-    if ret  == 0:
+def add_jh_student(course_id, student: User):
+    # add student to nbgrader database
+    ret = run_as_user(get_username(), os.getcwd(), 'nbgrader', 'db', 'student', 'add', '--first-name', student.first_name,
+                      '--last-name', student.last_name, '--email', student.email, student.id)
+
+    if ret == 0:
         prGreen('Successfully added {} to nbgrader database'.format(student.id))
-    
+
+
 def add_students(course_id, students_csv, jhub):
     students = []
-    with open(students_csv,'r') as f:
+    with open(students_csv, 'r') as f:
         csv_reader = csv.reader(f, delimiter=',')
         header = next(csv_reader)
 
@@ -228,7 +245,7 @@ def add_students(course_id, students_csv, jhub):
             if col not in cols_dict:
                 prRed('Missing column {} in {}.'.format(col, students_csv))
                 sys.exit(1)
-            
+
         for i, row in enumerate(csv_reader):
             student_dict = {}
             student_id = row[cols_dict['student_id']]
@@ -239,7 +256,7 @@ def add_students(course_id, students_csv, jhub):
             last_name = row[cols_dict['last_name']]
             email = row[cols_dict['email']]
 
-            student_dict['username'] = student_id 
+            student_dict['username'] = student_id
             student_dict['first_name'] = first_name
             student_dict['last_name'] = last_name
             student_dict['email'] = email
@@ -266,15 +283,17 @@ def add_students(course_id, students_csv, jhub):
                 else:
                     prRed('There was an error adding {} to {}: {}'.format(user, course_id, s['message']))
 
+
 def remove_student(course_id, student_id):
     url = '{}/student/{}/{}'.format(ngshare_url(), course_id, student_id)
-    data = {'user': get_username()} 
+    data = {'user': get_username()}
     response = delete(url, data)
-    
+
     if response is None:
         prRed('An error occurred while trying to delete {} from {}'.format(student_id, course_id))
     else:
         prGreen('Successfully deleted {} from {}'.format(student_id, course_id))
+
 
 def add_instructor(course_id, instructor: User):
     url = '{}/instructor/{}/{}'.format(ngshare_url(), course_id, instructor.id)
@@ -286,6 +305,7 @@ def add_instructor(course_id, instructor: User):
     else:
         prGreen('Successfully added {} as an instructor to {}'.format(instructor.id, course_id))
 
+
 def remove_instructor(course_id, instructor_id):
     url = '{}/instructor/{}/{}'.format(ngshare_url(), course_id, instructor_id)
     data = {'user': get_username()}
@@ -296,20 +316,23 @@ def remove_instructor(course_id, instructor_id):
     else:
         prGreen('Successfully deleted instructor {} from {}'.format(instructor_id, course_id))
 
+
 def parse_input(argv):
-    if len(argv) < 3:
-        print_usage()
+    if len(argv) < 1:
+        prRed('Please enter a valid command')
+        sys.exit(1)
 
     command = (argv[1]).lower()
     shortopts = 'c:s:i:f:l:e:'
-    longopts = 'course_id= student_id= instructor_id= first_name= last_name= email= students_csv= jhub'.split()
+    longopts = 'course_id= student_id= instructor_id= first_name= last_name= email= students_csv= jhub help'.split()
 
     try:
-        optlist, args = getopt.getopt(argv[2:], shortopts, longopts)
+        optlist, args = getopt.getopt(argv[1:], shortopts, longopts)
     except getopt.GetoptError:
         print_usage()
 
     return command, optlist
+
 
 def execute_command(command, optlist):
     course_id = None
@@ -338,6 +361,9 @@ def execute_command(command, optlist):
             students_csv = arg
         elif opt == '--jhub':
             jhub = True
+        elif opt == '--help':
+            print_usage()
+            return
 
     if command == 'create_course' and course_id:
         create_course(course_id, jhub)
@@ -354,12 +380,16 @@ def execute_command(command, optlist):
     elif command == 'remove_instructor' and course_id and instructor_id:
         remove_instructor(course_id, instructor_id)
     else:
-        print_usage()
+        prRed('The command you entered was not recognized.Use the --help flag to see usage examples')
+
 
 def main(argv=None):
+
     argv = argv or sys.argv
     command, optlist = parse_input(argv)
     execute_command(command, optlist)
 
+
 if __name__ == '__main__':
+
     sys.exit(main())
