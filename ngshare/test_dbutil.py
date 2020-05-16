227142e85c2f555cbe445a1ff211ed96cbe901e4
rkevin <rk@rkevin.dev>
2020-05-16 01:22:51 -0700

    don't modify sys.argv and remove /tmp/ngshare.db before testing

--- ngshare/test_dbutil.py
+++ ngshare/test_dbutil.py
@@ -2,30 +2,29 @@
     Test database migration tools
 '''
 
-import sys
 from . import dbutil
-
+import os
 
 def test_update():
     'Test update database'
+
+    # delete database if exist
+    try:
+        os.unlink(dbutil.DEFAULT_DB)
+    except FileNotFoundError:
+        pass
+
     dbutil.upgrade(dbutil.DEFAULT_DB)
-    sys_argv = sys.argv
     # Downgrade to init
-    sys.argv = ['dbutil.py', 'downgrade', 'aa00db20c10a']
-    dbutil.main()
+    dbutil.main(['downgrade', 'aa00db20c10a'])
     # Downgrade to nothing
-    sys.argv = ['dbutil.py', 'downgrade', 'base']
-    dbutil.main()
+    dbutil.main(['downgrade', 'base'])
     # Offline mode
-    sys.argv = ['dbutil.py', 'upgrade', 'head', '--sql']
-    dbutil.main()
+    dbutil.main(['upgrade', 'head', '--sql'])
     # Upgrade to head
-    sys.argv = ['dbutil.py', 'upgrade', 'head']
-    dbutil.main()
+    dbutil.main(['upgrade', 'head'])
     # Invalid argument error
     try:
-        sys.argv = ['dbutil.py']
-        dbutil.main()
+        dbutil.main([])
     except SystemExit:
         pass
-    sys.argv = sys_argv
